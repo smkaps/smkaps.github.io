0ce2c53da3f48c4cd0604f6b4c6a57161027baa3
Simon Kaplan <smkaps@gmail.com>
2023-04-14 13:18:58 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+  $("#cvbutton").click(function(){showCV()})
+  $("#pubbutton").click(function(){showpubs()})
+  $("#homebutton").click(function(){gohome()})
+  $("#links").click(function(){showlinks()})
+
+
+});
+
+function showCV(){
+  $(".block4").show(); // show cv iframe
+  $(".block2").hide(); // hide bio
+  $(".block3").hide(); // hide publications list
+  $(".block5").hide(); // hide links
+
+}
+
+function showpubs(){
+  $(".block3").show(); // show publication list
+  $(".block2").hide(); // hide bio
+  $(".block4").hide(); // hide cv iframe
+  $(".block5").hide(); // hide links
+}
+
+function gohome(){
+  $(".block2").show(); // show bio
+  $(".block3").hide(); // hide publication list
+  $(".block4").hide(); // hide cv frame
+  $(".block5").hide(); // hide links
+
+}
+
+function showlinks(){
+  $(".block5").show(); // hide links
+  $(".block2").hide(); // show bio
+  $(".block3").hide(); // hide publication list
+  $(".block4").hide(); // hide cv frame
+
+}
