e33bcef19b61982f17c72e67fbacdf720a1e0159
Simon Kaplan <smkaps@gmail.com>
2023-06-23 13:07:28 -0500

    623

--- index.js
+++ index.js
@@ -10,14 +10,12 @@ $(document).ready(function(){
 function showCV(){
   $(".block4").show(); // show cv iframe
   $(".block2").hide(); // hide bio
-  $(".block3").hide(); // hide publications list
   $(".block5").hide(); // hide links
 
 }
 
 function gohome(){
   $(".block2").show(); // show bio
-  $(".block3").hide(); // hide publication list
   $(".block4").hide(); // hide cv frame
   $(".block5").hide(); // hide links
 
@@ -26,7 +24,6 @@ function gohome(){
 function showlinks(){
   $(".block5").show(); // hide links
   $(".block2").hide(); // show bio
-  $(".block3").hide(); // hide publication list
   $(".block4").hide(); // hide cv frame
 
 }
